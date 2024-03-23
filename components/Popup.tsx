204a3f8aef207ed235f438d4d51e5a18d045345f
yasuomang <mchfollow@gmail.com>
2024-03-23 12:10:02 +0800

    style: popup

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -22,6 +22,7 @@ const PopUp = ({ openPopUp, closePopUp, children }: Props) => {
       id="ModelContainer"
       onClick={handlelosePopUp}
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-20 backdrop-blur-sm"
+      style={{ marginLeft: 0 }}
     >
       <div className="lg:1/3 w-10/12 rounded-lg border-e-emerald-600 bg-white p-2 py-5 shadow-inner md:w-1/2">
         <div className="w-full items-center justify-center p-3">{children}</div>
